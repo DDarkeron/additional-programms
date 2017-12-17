b457d0adae76550ac583cf685b9947fdeedfa994
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-17 22:23:14 +0300

    Update serv.c

--- serv.c
+++ serv.c
@@ -14,25 +14,25 @@
 int main()
 {
 	char server_message[256] = "You have reached the server!";
-    //create the server socket
+    
 	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	//define server address
+	
 	struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
     server_address.sin_addr.s_addr = INADDR_ANY;
 	
-	//bind the socket to our spicified IP and port
-	bind(server_socket,(struct sockaddr *), &server_address, sizeof(server_address));
+	
+	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 	
 	listen(server_socket, 10);
 	
 	int client_socket;
-	int client_socket = accept(server_socket, (struct sockaddr *), sizeof(server_address));
+	client_socket = accept(server_socket, NULL , NULL);
 	
 	
-	send(client_socket, server_message, siZeof(server_message), 0);
+	send(client_socket, server_message, sizeof(server_message), 0);
 	
 	close(server_socket);
 
