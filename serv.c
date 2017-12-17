b71ba40cde45ce978a49fcf0fafb8146e954949d
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-18 00:30:37 +0300

    Update serv.c

--- serv.c
+++ serv.c
@@ -11,60 +11,53 @@
 #include <fcntl.h>
 #include <sys/sendfile.h>
 
-int main()
+
+int main(int argc, char *argv[])
 {
 	FILE *file;
-	file fopen("index.html","r");
-	char response_data[1024];
-	fgets(response_data, 1024, file);
+	file = fopen("index.html","r");
+	char response_data[128];
+	fgets(response_data, 128, file);
 	
-	char header[2048] = "HTTP/1.1 200 OK\r\n\r\n";
-	//unite the string
-	strcat(header, response_data);
+	char http_header[256] = "HTTP/1.1 200 OK\r\n\n";
+	
+	strcat(http_header, response_data);
 	
 	int server_socket;
 	
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	struct sockaddr_in server_address;
-	server_address.sin_famile = AF_INET;
+	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(8080);
-	server_address.sin_addr = INADDR_ANY;
+	server_address.sin_addr.s_addr = INADDR_ANY;
 	
-	bind(server_socket, (struct sockaddr*) &server_address, sizeod(server_address));
-	
-	listen(server_socket, 10);
+	if( bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address))== -1 )
+        {
+        perror("can't bind");
+        close(server_socket);
+        exit(1);
+	}
+	if(listen(server_socket, 10)== -1)
+        {
+        perror("can't listen");
+        close(server_socket);
+        exit(1);
+        }
+
 	int client_socket;
 	while(1)
 	{
-	client_socket = accept(server_socket, NULL , NULL);
-	send(client_socket, header, sizeof(header), 0);
-	close(client_socket);	
+	client_socket = accept(server_socket, NULL, NULL);
+        if(client_socket == -1){
+        perror("Connection failed...\n");
+        continue; 
+
+        }
+        printf("Got connection...\n");
+	send(client_socket, http_header, sizeof(http_header), 0);
+        close(client_socket);	
 	}
-	
-	
-	/*
-	char server_message[256] = "You have reached the server!";
-    
-	int server_socket;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	
-	struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-	
-	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-	
-	listen(server_socket, 10);
-	
-	int client_socket;
-	client_socket = accept(server_socket, NULL , NULL);
-	
-	
-	send(client_socket, server_message, sizeof(server_message), 0);
-	
-	close(server_socket);*/
 	
 return 0;	
 }
